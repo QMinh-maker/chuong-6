5e69ffa3da53fe4494bd8443ce08ec0af3a42fa5
Minh <qminh2702@gmail.com>
2025-07-10 09:07:26 +0700

    Demo 8

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class EnemyWave 
+    {
+        public Transform enemyPrefab;
+        public int numberOfEnemy;
+        public Vector3 formationOffset;
+        public FlyPath flyPath;
+        public float speed;
+        public float nextWaveDelay;
+    }
+    public EnemyWave[] enemyWaves;
+    private int currentWave;
+    void Start()
+    {
+        SpawnEnemyWave();
+    }
+
+    // Update is called once per frame
+    private void SpawnEnemyWave()
+    {
+        var waveInfo = enemyWaves[currentWave];
+        var startPosition = waveInfo.flyPath[0];
+        
+        for (int i = 0; i < waveInfo.numberOfEnemy; i++)
+        {
+            var enemy = Instantiate(waveInfo.enemyPrefab, startPosition, Quaternion.identity);
+            var agent = enemy.GetComponent<FlyPathAgent>();
+            agent.flyPath = waveInfo.flyPath;
+            agent.flySpeed = waveInfo.speed;
+            startPosition += waveInfo.formationOffset;
+        }
+
+        currentWave++;
+        if (currentWave < enemyWaves.Length)
+        {
+            Invoke(nameof(SpawnEnemyWave), waveInfo.nextWaveDelay);
+        }
+
+    }
+}
--- Assets/_assets/FlyPath.cs
+++ Assets/_assets/FlyPath.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class FlyPath : MonoBehaviour
 {
     public WayPoint[] waypoints;
+    public Vector3 this[int index] => waypoints[index].transform.position;
 
     private void Reset()
     {
         waypoints = GetComponentsInChildren<WayPoint>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (waypoints == null) return;
+
+        Gizmos.color = Color.green;
+        for (int i = 0; i < waypoints.Length - 1; i++)
+        {
+            Gizmos.DrawLine(waypoints[i].transform.position, waypoints[i + 1].transform.position);
+        }
     }
 }
